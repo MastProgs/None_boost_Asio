d95db363cda1b57ee1af734be0a59a8ac5bc950b
MastProgs <khj2804@kpu.ac.kr>
2021-06-07 17:50:38 +0900

    added format func

--- C++20 Asio/AsioCpp20/Packet/PacketHandler.cpp
+++ C++20 Asio/AsioCpp20/Packet/PacketHandler.cpp
@@ -1,15 +1,16 @@
 #include "stdafx.h"
 #include "PacketHandler.h"
 #include <Core/AsioClient.h>
+#include <format>
 
 void PacketHandler::ProcessPacket(const std::string_view& packet, std::shared_ptr<AsioClient> client)
 {
-	std::cout << "PacketHandler : " << packet << "\n";
+	std::cout << std::format("C2S_PacketHandler : {}\n", packet);
 	client->SendPacket(packet);
 }
 
 void C2S_PacketHandler::ProcessPacket(const std::string_view& packet, std::shared_ptr<GameClient> client)
 {
-	std::cout << "C2S_PacketHandler : " << packet << "\n";
+	std::cout << std::format("C2S_PacketHandler : {}\n", packet);
 	client->SendPacket(packet);
 }
