89da81394be661d43378898b0d33f118a998a3dd
김형준 <zelda@wemade.com>
2022-07-01 16:32:40 +0900

    Update Rand.h

--- C++20 Asio/CoroutineAsioCpp20/Common/Random/Rand.h
+++ C++20 Asio/CoroutineAsioCpp20/Common/Random/Rand.h
@@ -6,44 +6,34 @@ public:
 	Rand() {};
 	~Rand() {};
 
-	int Get(int end)
-	{
-		return Get(0, end);
-	};
-
-	int Get(int start, int end)
+	int Dice(int end) { return Dice(0, end); };
+	int Dice(int start, int end)
 	{
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<int> dis(start, end);
-
 		return dis(gen);
 	};
 
 	template<typename T>
-	T& Get(std::vector<T>& vec)
+	T& Dice(std::vector<T>& vec)
 	{
-		auto idx = Get(0, vec.size() - 1);
+		auto idx = Dice(0, vec.size() - 1);
 		return vec[idx];
 	}
 
-	bool BoolDice(int percent, bool expect, int maxP)
+public:
+	bool BDice(int percent, bool expect, int maxP)
 	{
 		percent = ValidCheck(percent, maxP);
 		auto fixP = expect == false ? percent : (maxP - percent) * -1;
-		auto res = Get(maxP);
+		auto res = Dice(maxP);
 		return percent < res;
 	}
 
-	bool BoolDice100(int percent, bool expect = false, int maxP = 100)
-	{
-		return BoolDice(percent, expect, maxP);
-	}
+	bool BDice100(int percent, bool expect = false) { return BDice(percent, expect, 100); }
+	bool BDice10000(int percent, bool expect = false) { return BDice(percent, expect, 10000); }
 
-	bool BoolDice10000(int percent, bool expect = false, int maxP = 10000)
-	{
-		return BoolDice(percent, expect, maxP);
-	}
 private:
 	int ValidCheck(int val, int max, int min = 0)
 	{
